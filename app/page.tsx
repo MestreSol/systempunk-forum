a0c9befdc18ffbe0d7dfcbb38be143c0b007dcc5
MestreSol <mb.muitobosta@gmail.com>
2024-12-09 16:13:52 -0300

    🎨 style: refactor SuperiorMenu component for improved layout and styling using Bootstrap

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <>
-      <SuperiorMenu></SuperiorMenu>
+      <SuperiorMenu />
     </>
   );
 }
--- components/superior-menu/page.tsx
+++ components/superior-menu/page.tsx
@@ -1,42 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './styles.module.css';
 
 const SuperiorMenu = () => {
   return (
-    <nav className={styles.SuperiorMenu}>
-      <div>
-
-        <div className={styles.BrandArea}>
-          <img src="/vercel.svg"></img>
-          <h1>Systempunk</h1>
+      <div className="container">
+        <nav className={`navbar navbar-expand-lg navbar-dark bg-dark ${styles.SuperiorMenu}`}>
+        <div className="navbar-brand d-flex align-items-center">
+          <img src="/vercel.svg" className="d-inline-block align-top" width="40" height="40" alt="Brand logo" />
+          <h1 className="ml-2">Systempunk</h1>
         </div>
-        <ul className={styles.LinkList}>
-          <li className={styles.ListItem}>
-            <a>
-              Home
-            </a>
-          </li>
-          <li className={styles.ListItem}>
-            <a>
-              Categories
-            </a>
-          </li>
-          <li className={styles.ListItem}>
-            <a>
-            Timeline
-            </a>
-          </li>
-          <li className={styles.ListItem}>
-            <a>
-              Contribute
-            </a>
-          </li>
-        </ul>  
-        <div>
-          <img src="/file.svg" />
-          <p>User</p>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Categories</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Timeline</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contribute</a>
+            </li>
+          </ul>
         </div>
-      </div>
+        <div className="d-flex align-items-center">
+          <p className="mb-0 mr-2">User</p>
+          <img src="/file.svg" width="40" height="40" alt="User icon" />
+        </div>
     </nav>
+    </div>
   );
 };
 
