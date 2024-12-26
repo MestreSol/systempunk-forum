9d253fa6300b6e521b0ab39035cb266b5594d21a
MestreSol <joaovitorferreira8000@gmail.com>
2024-12-26 13:40:04 -0300

    feat: add Carousel component with styles and integrate it into the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { GlobalStyle } from '@/global/GlobalStyle'
+import Carousel from '@/components/Carousel'
 
 export default function Home() {
   return (
     <div className={styles.page}>
         <GlobalStyle></GlobalStyle>
-
+        <Carousel></Carousel>
     </div>
   )
 }
