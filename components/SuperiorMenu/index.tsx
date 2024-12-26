6c0eeef704656dbd8eba4884dd5d0b4a7ff26d1b
MestreSol <joaovitorferreira8000@gmail.com>
2024-12-26 13:23:03 -0300

    feat: add Timeline page, global styles, and SuperiorMenu component; update Prettier and EditorConfig settings

---
+++ components/SuperiorMenu/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './style.css';
+export default function SuperiorMenu(){
+    return (
+        <nav className="superior-menu">
+            <div className="brand-area">
+                <img src="logo.png"></img>
+                <h1>Systempunk</h1>
+            </div>
+            <div className="menu-area">
+                <ul>
+                    <li className="menu-item">
+                        <a href="#">Home</a>
+                    </li>
+                    <li className="menu-item">
+                        <a href="#">Universe</a>
+                    </li>
+                    <li className="menu-item">
+                        <a href="#">Timeline</a>
+                    </li>
+                    <li className="menu-item">
+                        <a href="#">Projects</a>
+                    </li>
+                    <li className="menu-item">
+                        <a href="#">Contribute</a>
+                    </li>
+                    <li className="menu-item">
+                        <a href="#">About</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="user-area">
+                <a href="#">Login</a>
+            </div>
+        </nav>
+    )
+}
---
+++ global/Theme.ts
@@ -0,0 +1,49 @@
+export default {
+    Colors:{
+        primary: "#000000",
+        secondary: "#FFFFFF",
+        tertiary: "#FF0000",
+        text: "#000000",
+        background: "#420000",
+        border: "#000000",
+    },
+    fontSize:{
+        small: "12px",
+        medium: "16px",
+        large: "20px",
+    },
+    fontFamily:{
+        primary: "Arial",
+        secondary: "Arial",
+    },
+    spacing:{
+        small: "4px",
+        medium: "8px",
+        large: "16px",
+    },
+    borderRadius:{
+        small: "4px",
+        medium: "8px",
+        large: "16px",
+    },
+    borderWidth:{
+        small: "1px",
+        medium: "2px",
+        large: "4px",
+    },
+    shadow:{
+        small: "0 0 4px rgba(0,0,0,0.1)",
+        medium: "0 0 8px rgba(0,0,0,0.1)",
+        large: "0 0 16px rgba(0,0,0,0.1)",
+    },
+    transition:{
+        small: "0.2s",
+        medium: "0.4s",
+        large: "0.8s",
+    },
+    breakpoint:{
+        mobile: "640px",
+        tablet: "768px",
+        pc: "1024px",
+    },
+}
