991c90b60080988d553fc8f07243e13cd6e3abcf
MestreSol <joaovitorferreira8000@gmail.com>
2024-12-26 19:09:37 -0300

    feat: add MoustRecent container and update Home component layout; enhance Card styles for better responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,17 @@
 import styles from './page.module.css'
 import { GlobalStyle } from '@/global/GlobalStyle'
 import Carousel from '@/components/Carousel'
-import Card from '@/components/Card'
 import './globals.css'
 import MainHub from '@/containers/MainHub'
 import Footer from '@/containers/Footer'
+import MoustRecent from '@/containers/MoustRecent'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <GlobalStyle />
       <Carousel />
-      <div className={styles.cards}>
-        <Card />
-        <Card />
-      </div>
+      <MoustRecent></MoustRecent>
       <MainHub></MainHub>
       <Footer></Footer>
     </div>
---
+++ containers/MoustRecent/index.tsx
@@ -0,0 +1,12 @@
+import Card from "@/components/Card";
+
+const MoustRecent = () => {
+    return (
+        <div className="card-area">
+            <Card></Card>
+            <Card></Card>
+        </div>
+    );
+}
+
+export default MoustRecent;
