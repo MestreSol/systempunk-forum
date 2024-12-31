df157a7ccb82f31b96d266c6de1be7b7a8984b9c
MestreSol <mb.muitobosta@gmail.com>
2024-12-31 02:52:51 -0300

    feat: add JSON server setup and update component styles; improve code formatting and consistency

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,59 +1,120 @@
-"use client"
-import React from 'react'
-import { GlobalStyle } from "@/global/GlobalStyle"
+'use client'
+import React, { useEffect, useState } from 'react'
+import { GlobalStyle } from '@/global/GlobalStyle'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
-import { Navigation, Pagination } from "swiper/modules"
-import "./styles.css"
+import { Navigation, Pagination } from 'swiper/modules'
+import './styles.css'
 
+interface Punk {
+  id: number
+  name: string
+  data: string
+  image: string
+}
 export default function TimelinePage() {
+  const [punks, setPunks] = useState<Punk[]>([])
   const numberOfSlides = 5
   const panels = Array.from({ length: numberOfSlides }, (_, i) => i + 1)
 
+  useEffect(() => {
+    fetch('http://localhost:3001/punks')
+      .then((response) => response.json())
+      .then((data) => setPunks(data))
+  }, [])
+
   const handleSlideChange = (swiper: any) => {
     swiper.slides.forEach((slide: HTMLElement) => {
-      let background = slide.querySelector(".background")
+      let background = slide.querySelector('.background')
       if (background) {
-        background.classList.remove("animation")
+        background.classList.remove('animation')
       }
     })
     let activeSlide = swiper.slides[swiper.activeIndex]
     if (activeSlide) {
-      let background = activeSlide.querySelector(".background")
+      let background = activeSlide.querySelector('.background')
       if (background) {
-        background.classList.add("animation")
+        background.classList.add('animation')
       }
     }
   }
 
   return (
-    <div className='timeline-page'>
+    <Swiper
+      direction="vertical"
+      effect="fade"
+      speed={1000}
+      loop={true}
+      pagination={{ clickable: true }}
+      mousewheel={{
+        invert: false,
+        forceToAxis: false,
+        thresholdDelta: 50,
+        sensitivity: 1
+      }}
+      onSlideChange={handleSlideChange}
+      modules={[Navigation, Pagination]}
+    >
       <GlobalStyle />
-        <Swiper
-          direction="vertical"
-          effect="fade"
-          speed={1000}
-          loop={true}
-          pagination={{ clickable: true }}
-          mousewheel={{ invert: false, forceToAxis: false, thresholdDelta: 50, sensitivity: 1 }}
-          onSlideChange={handleSlideChange}
-          modules={[Navigation, Pagination]}
-        >
-          <SwiperSlide>
-            <div className="content" data-content="five">
-              <h1>New York, USA</h1>
-              <p>
-                Experience New York City, where excitement and adventure await at
-                every corner. See the Statue of Liberty, enjoy a Broadway show,
-                and taste food from around the world. This tour is ideal for those
-                who want to feel the energy of a big city.
-              </p>
-            </div>
-          <div className="background" data-item="five"></div>
-          </SwiperSlide>
-        </Swiper>
-    </div>
+
+      <SwiperSlide key="1">
+        <div className="content" data-content="1">
+          <h1>Apunk</h1>
+          <p>
+            As três [[Supra-Singularidade]], criadas pelo [[Desconhecido]],
+            vagaram pelo vazio, cada uma com um fragmento de seu poder. Unidas,
+            deram origem à chama da existência, moldando-a e criando os 12
+            aspectos, artefatos que moldam a realidade. Espalhados pelo
+            universo, esses artefatos convergiram na Terra, dando origem à
+            [[Profecia do Fim]]. Na [[Grande Queda]], as [[Divindades]]
+            realizaram um sacrifício final, criando os [[Abismos da sombra]]
+            para aprisionar a [[Quimera]], encerrando a era com a batalha
+            decisiva.
+          </p>
+        </div>
+        <div
+          className="background"
+          data-item="1"
+          style={{ backgroundImage: `url(/punks/Apunk.png)` }}
+        ></div>
+      </SwiperSlide>
+      <SwiperSlide key="2">
+        <div className="content" data-content="2">
+          <h1>Stonepunk</h1>
+          <p>
+            As três [[Supra-Singularidade]], criadas pelo [[Desconhecido]],
+            vagaram pelo vazio, cada uma com um fragmento de seu poder. Unidas,
+            deram origem à chama da existência, moldando-a e criando os 12
+            aspectos, artefatos que moldam a realidade. Espalhados pelo
+            universo, esses artefatos convergiram na Terra, dando origem à
+            [[Profecia do Fim]]. Na [[Grande Queda]], as [[Divindades]]
+            realizaram um sacrifício final, criando os [[Abismos da sombra]]
+            para aprisionar a [[Quimera]], encerrando a era com a batalha
+            decisiva.
+          </p>
+        </div>
+        <div
+          className="background"
+          data-item="2"
+          style={{ backgroundImage: `url(/punks/Stonepunk.png)` }}
+        ></div>
+      </SwiperSlide>
+      {punks.map((punk) => (
+        <SwiperSlide key={punk.id}>
+          <div className="content" data-content={punk.id}>
+            <h1>{punk.name}</h1>
+            <p>{punk.data}</p>
+            <img src={punk.image} alt={punk.name} />
+          </div>
+          <div
+            className="background"
+            data-item={punk.id}
+            style={{ backgroundImage: `url(${punk.image})` }}
+          ></div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   )
 }
--- containers/MoustRecent/index.tsx
+++ containers/MoustRecent/index.tsx
@@ -1,15 +1,15 @@
-import Card from "@/components/Card";
-import "./styles.css";
+import Card from '@/components/Card'
+import './styles.css'
 const MoustRecent = () => {
-    return (
-        <div className="most-recent">
-            <h2>Most Recent</h2>
-        <div className="card-area">
-            <Card></Card>
-            <Card></Card>
-        </div>
-        </div>
-    );
+  return (
+    <div className="most-recent">
+      <h2>Most Recent</h2>
+      <div className="card-area">
+        <Card></Card>
+        <Card></Card>
+      </div>
+    </div>
+  )
 }
 
-export default MoustRecent;
+export default MoustRecent
