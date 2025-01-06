005f9beb5563adfb631f3f0be314d910d968c5d6
MestreSol <mb.muitobosta@gmail.com>
2025-01-06 19:45:05 -0300

    feat: add Universe component and remove server.js file

---
+++ app/Universe/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { GlobalStyle } from "@/global/GlobalStyle"
+
+export default function Universe() {
+  return (
+    <div>
+      <GlobalStyle />
+      <h1>Universe</h1>
+    </div>
+  )
+}
