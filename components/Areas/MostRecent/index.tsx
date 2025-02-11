3bca842e9d3e1fc5e69084282d3b65eb06b0cf73
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2025-02-11 18:54:57 -0300

    feat: rename MoustRecent component to MostRecent and add styles

---
+++ components/Areas/MostRecent/index.tsx
@@ -0,0 +1,13 @@
+import Card from '@/components/Areas/Card'
+import style from './style.module.scss'
+
+const MostRecent = () => {
+  return (
+    <div className={style.mostRecent}>
+      <Card></Card>
+      <Card></Card>
+    </div>
+  )
+}
+
+export default MostRecent
