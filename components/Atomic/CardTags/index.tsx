5448f1411bb48b7081a134e3888c79d0f3ee1c60
MestreSol <mb.muitobosta@gmail.com>
2025-02-12 11:13:22 -0300

    feat: add CardTags component for displaying tags in the card

---
+++ components/Atomic/CardTags/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CardTags = () => {
+  return (
+    <div className="card-tags">
+      <span className="tag">Tag1</span>
+      <span className="tag">Tag2</span>
+      <span className="tag">Tag3</span>
+    </div>
+  )
+}
+
+export default CardTags
