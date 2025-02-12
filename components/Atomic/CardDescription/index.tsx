3e3d2f4beb1806d0b8dd71a17fde32c922ba207b
MestreSol <mb.muitobosta@gmail.com>
2025-02-12 11:13:13 -0300

    feat: add CardDescription component for displaying descriptive text

---
+++ components/Atomic/CardDescription/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CardDescription = () => {
+  return (
+    <p className="card-description">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+      scelerisque ante lorem, pretium faucibus dolor ornare nec. Donec elit
+      sapien, ornare at ex aliquam, consequat semper magna. Nam sit amet
+      consequat urna. Vivamus nec nisi enim. Vestibulum lobortis sem sed justo
+      imperdiet, non finibus massa viverra.
+    </p>
+  )
+}
+
+export default CardDescription
