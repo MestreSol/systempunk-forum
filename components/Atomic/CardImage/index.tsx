fa55c8c8ce7d1d613dba62c7d526b08b15563bfc
MestreSol <mb.muitobosta@gmail.com>
2025-02-12 11:13:18 -0300

    feat: add CardImage component for displaying an image in the card

---
+++ components/Atomic/CardImage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CardImage = () => {
+  return (
+    <div className="card-right">
+      <img
+        src="https://cdn.mos.cms.futurecdn.net/dP3N4qnEZ4tCTCLq59iysd.jpg"
+        alt="Card Image"
+      />
+    </div>
+  )
+}
+
+export default CardImage
