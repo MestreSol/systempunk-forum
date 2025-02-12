11b4293ec61225609a8e7c4d7e93459a8f88b7f3
MestreSol <mb.muitobosta@gmail.com>
2025-02-12 11:12:24 -0300

    feat: refactor Card component to use atomic design pattern and add new styles for Sidebar and UpdateSection

--- components/Areas/Card/index.tsx
+++ components/Areas/Card/index.tsx
@@ -1,27 +1,23 @@
+import React from 'react'
 import './styles.css'
+import CardTags from '@/components/Atomic/CardTags'
+import CardTitle from '@/components/Atomic/CardTitle'
+import CardData from '@/components/Atomic/CardData'
+import CardDescription from '@/components/Atomic/CardDescription'
+import CardButton from '@/components/Atomic/CardButton'
+import CardImage from '@/components/Atomic/CardImage'
+
 const Card = () => {
   return (
     <div className="card">
       <div className="card-left">
-        <div className="card-tags">
-          <span className="tag">Tag1</span>
-          <span className="tag">Tag2</span>
-          <span className="tag">Tag3</span>
-        </div>
-        <h1 className="card-title">Lorem Ipsum</h1>
-        <p className="card-data">12/2024</p>
-        <p className="card-description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-          scelerisque ante lorem, pretium faucibus dolor ornare nec. Donec elit
-          sapien, ornare at ex aliquam, consequat semper magna. Nam sit amet
-          consequat urna. Vivamus nec nisi enim. Vestibulum lobortis sem sed
-          justo imperdiet, non finibus massa viverra.{' '}
-        </p>
-        <button className="card-button">Read More</button>
+        <CardTags />
+        <CardTitle />
+        <CardData />
+        <CardDescription />
+        <CardButton />
       </div>
-      <div className="card-right">
-        <img src="https://cdn.mos.cms.futurecdn.net/dP3N4qnEZ4tCTCLq59iysd.jpg"></img>
-      </div>
+      <CardImage />
     </div>
   )
 }
