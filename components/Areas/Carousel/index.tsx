8021cd81ea9982937c8a21cb5bf6ca851ac47ce0
MestreSol <mb.muitobosta@gmail.com>
2025-02-12 12:31:34 -0300

    feat: refactor Carousel component to use CSS modules and improve styling

--- components/Areas/Carousel/index.tsx
+++ components/Areas/Carousel/index.tsx
@@ -1,19 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import React, { useEffect } from 'react'
-import './styles.css'
+import style from './style.module.css'
 
 export default function Carousel() {
   useEffect(() => {
-    const slider = document.querySelector('.slider')
+    const slider = document.querySelector('#slider')
 
     function activate(e: Event) {
-      const items = document.querySelectorAll('.item')
+      const items = document.querySelectorAll('#item')
       if (!slider) return
-      if ((e.target as Element).matches('.next')) {
+      if ((e.target as Element).matches('#next')) {
         slider.append(items[0])
       }
-      if ((e.target as Element).matches('.prev')) {
+      if ((e.target as Element).matches('#prev')) {
         slider.prepend(items[items.length - 1])
       }
     }
@@ -21,17 +21,18 @@ export default function Carousel() {
     document.addEventListener('click', activate, false)
   }, [])
   return (
-    <main>
-      <ul className="slider">
+    <main className={style.container}>
+      <ul className={style.slider} id={'slider'}>
         <li
-          className="item"
+          className={style.item}
           style={{
             backgroundImage: `url('https://cdn.mos.cms.futurecdn.net/dP3N4qnEZ4tCTCLq59iysd.jpg')`
           }}
+          id={'item'}
         >
-          <div className="content">
-            <h2 className="title">Culturas do Systempunk</h2>
-            <p className="description">
+          <div className={style.content}>
+            <h2 className={style.title}>Culturas do Systempunk</h2>
+            <p className={style.description}>
               {' '}
               No universo de Systempunk, culturas evoluem e colidem, moldadas
               pela tecnologia, pelas crenças e pelo caos de cada era. De
@@ -44,14 +45,15 @@ export default function Carousel() {
           </div>
         </li>
         <li
-          className="item"
+          className={style.item}
           style={{
             backgroundImage: `url('https://i.redd.it/tc0aqpv92pn21.jpg')`
           }}
+          id={'item'}
         >
-          <div className="content">
-            <h2 className="title">Estrange Bond</h2>
-            <p className="description">
+          <div className={style.content}>
+            <h2 className={style.title}>Estrange Bond</h2>
+            <p className={style.description}>
               {' '}
               E se a grandiosidade de Dungeons & Dragons encontrasse a
               profundidade tecnológica e cultural do Systempunk? Neste artigo,
@@ -64,14 +66,15 @@ export default function Carousel() {
           </div>
         </li>
         <li
-          className="item"
+          className={style.item}
           style={{
             backgroundImage: `url('https://wharferj.files.wordpress.com/2015/11/bio_north.jpg')`
           }}
+          id={'item'}
         >
-          <div className="content">
-            <h2 className="title">Explore o Universo Systempunk</h2>
-            <p className="description">
+          <div className={style.content}>
+            <h2 className={style.title}>Explore o Universo Systempunk</h2>
+            <p className={style.description}>
               {' '}
               O Systempunk vai muito além de um conceito — é um universo vivo,
               repleto de histórias, jogos e mundos interconectados. Do suspense
@@ -80,18 +83,19 @@ export default function Carousel() {
               tecnológicas. Quer descobrir mais e se aprofundar nesse
               multiverso? Explore nossos projetos e escolha seu próximo destino!{' '}
             </p>
-            <button>Read More</button>
+            <a>Read More</a>
           </div>
         </li>
         <li
-          className="item"
+          className={style.item}
           style={{
             backgroundImage: `url('https://images7.alphacoders.com/878/878663.jpg')`
           }}
+          id={'item'}
         >
-          <div className="content">
-            <h2 className="title">O Passado e o Futuro Colidem</h2>
-            <p className="description">
+          <div className={style.content}>
+            <h2 className={style.title}>O Passado e o Futuro Colidem</h2>
+            <p className={style.description}>
               No Systempunk, cada era conta uma história de ascensão e queda,
               onde a tecnologia e a cultura se entrelaçam em um ciclo sem fim.
               De reinos arcaicos a civilizações movidas por máquinas
@@ -102,32 +106,34 @@ export default function Carousel() {
           </div>
         </li>
         <li
-          className="item"
+          className={style.item}
           style={{
             backgroundImage: `url('https://theawesomer.com/photos/2017/07/simon_stalenhag_the_electric_state_6.jpg')`
           }}
+          id={'item'}
         >
-          <div className="content">
-            <h2 className="title">Escolhas que Moldam Universos</h2>
-            <p className="description">
+          <div className={style.content}>
+            <h2 className={style.title}>Escolhas que Moldam Universos</h2>
+            <p className={style.description}>
               Aqui, não existem apenas heróis ou vilões, mas sociedades inteiras
               lutando para sobreviver e evoluir. Governos colapsam, corporações
               dominam, conhecimentos ancestrais ressurgem – e cada decisão pode
               alterar o curso da história. Você está pronto para mergulhar nos
               múltiplos caminhos do Systempunk?
             </p>
-            <button>Read More</button>
+            <a>Read More</a>
           </div>
         </li>
         <li
-          className="item"
+          className={style.item}
           style={{
             backgroundImage: `url('https://da.se/app/uploads/2015/09/simon-december1994.jpg')`
           }}
+          id={'item'}
         >
-          <div className="content">
-            <h2 className="title">Seu Portal para Novas Aventuras</h2>
-            <p className="description">
+          <div className={style.content}>
+            <h2 className={style.title}>Seu Portal para Novas Aventuras</h2>
+            <p className={style.description}>
               {' '}
               Quer desvendar os segredos ocultos nas sombras do tempo? Construir
               sua própria narrativa em meio ao caos das eras? O Systempunk Forum
@@ -135,15 +141,19 @@ export default function Carousel() {
               exploram o desconhecido. Entre e expanda os limites do seu
               universo!{' '}
             </p>
-            <button>Read More</button>
+            <a>Read More</a>
           </div>
         </li>
       </ul>
-      <nav className="nav">
-        <div className="btn prev" data-name="arrow-back-outline">
+      <nav className={style.nav}>
+        <div className={style.btn} data-name="arrow-back-outline" id={'prev'}>
           <FontAwesomeIcon icon={faArrowLeft} />
         </div>
-        <div className="btn next" data-name="arrow-forward-outline">
+        <div
+          className={style.btn}
+          data-name="arrow-forward-outline"
+          id={'next'}
+        >
           <FontAwesomeIcon icon={faArrowRight} />
         </div>
       </nav>
