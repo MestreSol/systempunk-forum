0225c7eeadacd87954138119714b981cb8c3fda3
MestreSol <mb.muitobosta@gmail.com>
2025-02-13 18:02:00 -0300

    Add Docker Compose

--- components/Atomic/MainContent/index.tsx
+++ components/Atomic/MainContent/index.tsx
@@ -2,15 +2,23 @@ import React from 'react'
 import style from './style.module.css'
 import UpdateSection from '@/components/Atomic/UpdateSection'
 
-const MainContent = () => {
+type Props = {
+  update: {
+    title: string
+    Date: string
+    MadeBy: string
+    Description: string
+    Content: string
+  }
+}
+
+const MainContent = ({ update }: Props) => {
   return (
     <div className={style.content}>
-      <h2 className={style.contentTitle}>Update Area</h2>
-      <h1 className={style.contentSubTitle}>
-        📢 Systempunk Forum - Atualização Inicial
-      </h1>
+      <h2 className={style.contentTitle}>{update.title}</h2>
+      <h1 className={style.contentSubTitle}>{update.Description}</h1>
       <p className={style.updateTime}>February 11, 2025 by MestreSol</p>
-      <UpdateSection />
+      <UpdateSection content={update.Content} />
     </div>
   )
 }
