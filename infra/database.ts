3b44929090ba6a63aa34d9724a21d4691f0b65ea
MestreSol <mb.muitobosta@gmail.com>
2025-02-14 13:01:15 -0300

    Udpate infra content

--- infra/database.ts
+++ infra/database.ts
@@ -2,11 +2,11 @@ import { Client } from 'pg'
 
 async function query(query: string) {
   const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'root',
-    port: 5432
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT || '5432', 10)
   })
   await client.connect()
   const res = await client.query(query)
