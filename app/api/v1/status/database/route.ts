ba2a3ffddad80dc0b4d4dce998d02de82d37b166
MestreSol <mb.muitobosta@gmail.com>
2025-02-21 15:54:02 -0300

    feat: update database configuration and enhance error handling in query functions

--- app/api/v1/status/database/route.ts
+++ app/api/v1/status/database/route.ts
@@ -2,8 +2,17 @@ import database from '@/infra/database'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const result = await database.query('SELECT version();')
-  const dbVersion = result.rows[0].version
-  console.log(dbVersion)
-  return NextResponse.json({ status: 'OK' }, { status: 200 })
+  return NextResponse.json({
+    update_at: new Date(),
+    dependencies: {
+      database: {
+        version: await database.getServerVersion(),
+        active_connections: Number.parseInt(
+          await database.getActiveConnections()
+        ),
+        max_connections: Number.parseInt(await database.getMaxConnections()),
+        timezone: await database.getTimezone()
+      }
+    }
+  })
 }
--- infra/database.ts
+++ infra/database.ts
@@ -9,9 +9,67 @@ async function query(query: string) {
     port: parseInt(process.env.POSTGRES_PORT || '5432', 10)
   })
   await client.connect()
-  const res = await client.query(query)
-  await client.end()
-  return res
+  try {
+    const res = await client.query(query)
+    return res
+  } catch (e) {
+    console.log(e)
+  } finally {
+    await client.end()
+  }
 }
 
-export default { query: query }
+async function heartbeat() {
+  return query('SELECT 1')
+}
+
+async function getServerVersion() {
+  const res = await query('SHOW server_version')
+  if (res) {
+    return res.rows[0].server_version
+  }
+  throw new Error('Query failed')
+}
+
+async function getAllInformation() {
+  const res = await query('SELECT * FROM pg_stat_activity')
+  if (res) {
+    return res.rows
+  }
+  throw new Error('Query failed')
+}
+
+async function getActiveConnections() {
+  const res = await query('SELECT count(*) FROM pg_stat_activity')
+  if (res) {
+    return res.rows[0].count
+  }
+  throw new Error('Query failed')
+}
+
+async function getMaxConnections() {
+  const res = await query('SHOW max_connections')
+  if (res) {
+    return res.rows[0].max_connections
+  }
+  throw new Error('Query failed')
+}
+
+async function getTimezone() {
+  const res = await query('SHOW timezone')
+  if (res) {
+    console.log(res.rows[0])
+    return res.rows[0].TimeZone
+  }
+  throw new Error('Query failed')
+}
+
+export default {
+  query: query,
+  heartbeat: heartbeat,
+  getServerVersion: getServerVersion,
+  getAllInformation: getAllInformation,
+  getActiveConnections: getActiveConnections,
+  getMaxConnections: getMaxConnections,
+  getTimezone: getTimezone
+}
