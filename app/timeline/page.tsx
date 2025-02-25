d7535ce9b4443bf84a45823e18b460855a18868d
MestreSol <mb.muitobosta@gmail.com>
2025-02-25 16:34:39 -0300

    feat: implement side menu in TimelinePage and remove unused layout files

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -6,6 +6,7 @@ import 'swiper/css/pagination'
 import 'swiper/css/navigation'
 import { Navigation, Pagination, Mousewheel } from 'swiper/modules'
 import './styles.css'
+import Link from 'next/link'
 
 interface Punk {
   id: number
@@ -16,6 +17,7 @@ interface Punk {
 
 export default function TimelinePage() {
   const [punks, setPunks] = useState<Punk[]>([])
+  const [menuActive, setMenuActive] = useState(false)
 
   useEffect(() => {
     fetch('http://localhost:3000/api/v1/punks')
@@ -40,29 +42,67 @@ export default function TimelinePage() {
     }
   }
 
+  const toggleMenu = () => {
+    setMenuActive(!menuActive)
+  }
+
   return (
-    <Swiper
-      direction="vertical"
-      speed={1000}
-      loop={true}
-      pagination={{ clickable: true }}
-      mousewheel={{ forceToAxis: true }}
-      onSlideChange={handleSlideChange}
-      modules={[Navigation, Pagination, Mousewheel]}
-    >
-      {punks.map((punk) => (
-        <SwiperSlide key={punk.id}>
-          <div className="content" data-content={punk.id}>
-            <h1>{punk.name}</h1>
-            <p>{punk.data}</p>
-          </div>
-          <div
-            className="background"
-            data-item={punk.id}
-            style={{ backgroundImage: `url(${punk.image})` }}
-          ></div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <div className="hamburger" onClick={toggleMenu}>
+        &#9776;
+      </div>
+      <div className={`side-menu ${menuActive ? 'active' : ''}`}>
+        <div className="close-btn" onClick={toggleMenu}>
+          &times;
+        </div>
+        <ul>
+          <li className="menu-item">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="menu-item">
+            <Link href="/universe">Universe</Link>
+          </li>
+          <li className="menu-item">
+            <Link href="/timeline">Timeline</Link>
+          </li>
+          <li className="menu-item">
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li className="menu-item">
+            <Link href="/contribute">Contribute</Link>
+          </li>
+          <li className="menu-item">
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+      <Swiper
+        direction="vertical"
+        speed={1000}
+        loop={true}
+        pagination={{ clickable: true }}
+        mousewheel={{ forceToAxis: true }}
+        onSlideChange={handleSlideChange}
+        modules={[Navigation, Pagination, Mousewheel]}
+      >
+        <div className="menu"></div>
+        {punks.map((punk) => (
+          <SwiperSlide key={punk.id}>
+            <div className="content" data-content={punk.id}>
+              <h1>{punk.name}</h1>
+              <p>{punk.data}</p>
+              <Link href={`/universo/eras/${punk.name}`} className="readMore">
+                Read More
+              </Link>
+            </div>
+            <div
+              className="background"
+              data-item={punk.id}
+              style={{ backgroundImage: `url(${punk.image})` }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   )
 }
