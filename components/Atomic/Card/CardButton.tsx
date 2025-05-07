ec49b88b24421e31f244303da2979a7dea32906e
MestreSol <joaovitorferreira8000@gmail.com>
2025-05-07 09:55:56 -0300

    Remove unused files and configurations
    - Deleted package.json and tsconfig.json as they are no longer needed.
    - Removed various image files from the public directory, including social media icons and punk images.
    - Cleared out test files related to API endpoints for punks and status checks.
    - Removed SVG files that are no longer in use.

--- components/Atomic/Card/CardButton.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import style from './style.module.css'
-
-const CardButton = () => {
-  return <button className={style.cardButton}>Read More</button>
-}
-
-export default CardButton
--- components/Atomic/Card/CardTitle.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import style from './style.module.css'
-const CardTitle = () => {
-  return <h1 className={style.title}>Lorem Ipsum</h1>
-}
-
-export default CardTitle
