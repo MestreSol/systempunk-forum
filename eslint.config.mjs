9f18ab74a5366c3d17590e5ea4e6512dd91895be
Ferreira. João Vitor <joaovitorferreira8000@gmail.com>
2025-09-13 11:56:19 -0300

    chore: update package.json dependencies and version; remove tsconfig.eslint.json; add components.json configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,59 +1,33 @@
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
-export default tseslint.config(
-  {
-    ignores: [
-      'node_modules',
-      'dist',
-      'coverage',
-      'swagger-static',
-      'eslint.config.mjs',
-    ],
-  },
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import prettierPlugin from 'eslint-plugin-prettier'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
 
-  eslint.configs.recommended,
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-  ...tseslint.configs.recommendedTypeChecked,
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+})
 
-  eslintPluginPrettierRecommended,
-
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     plugins: {
-      'simple-import-sort': simpleImportSort,
-    },
-    languageOptions: {
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-      globals: {
-        ...globals.node,
-      },
+      prettier: prettierPlugin,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-
-      'simple-import-sort/imports': 'warn',
-      'simple-import-sort/exports': 'warn',
+      'prettier/prettier': ['error', { endOfLine: 'crlf' }],
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': 'off'
     },
-  },
-
-  {
-    files: ['**/*.spec.ts', '**/*.test.ts'],
-    languageOptions: {
-      globals: {
-        ...globals.jest,
-      },
-    },
-    rules: {
-      '@typescript-eslint/no-unsafe-argument': 'off',
-    },
+    ignores: ['.next', 'node_modules']
   }
-);
+]
+
+export default eslintConfig
